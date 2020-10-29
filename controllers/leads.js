954616b451d6c0fbc8fa49e5e6475aed2e9a29e6
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-10-29 08:59:18 +0200

    add lead

---
+++ controllers/leads.js
@@ -0,0 +1,23 @@
+const fse = require('fs-extra')
+
+
+exports.addLead = async function(req, res){
+  console.log(req.body)
+  var data = req.body;
+  data.createdAt = Date()
+  data.createdAtUS = Date.now()
+  var file;
+  try{
+    file = await fse.readJSONSync('/Users/ahmedmgh/WORK/DPLYR/Tech/utils/crm/data.json')
+    data.index = file.length
+    file.push(data)
+  } catch {
+    data.id = 0
+    file = [data]
+    console.log("file not found creating new file")
+  }
+  fse.writeJSONSync('/Users/ahmedmgh/WORK/DPLYR/Tech/utils/crm/data.json', file)
+  
+
+  return res.render('added', {text:"Hello "+ JSON.stringify(req.body)})
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var LCtrl = require('../controllers/leads')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'DPLYR CRM' });
 });
-
+router.get('/leads', function(req, res, next) {
+  res.render('all', { title: 'DPLYR CRM' });
+});
+router.post('/leads/add', LCtrl.addLead)
 module.exports = router;
