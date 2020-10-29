5b807da668ed880074fd7e0c35f648801eaae128
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-10-29 09:10:06 +0200

    view all leads

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var hbs = require('hbs');
+
+
+hbs.registerHelper('eachProperty', function(context, options) {
+  var ret = "";
+  for(var prop in context)
+  {
+      ret = ret + options.fn({property:prop,value:context[prop]});
+  }
+  return ret;
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,12 +35,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var LCtrl = require('../controllers/leads')
+const fse = require('fs-extra')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'DPLYR CRM' });
 });
-router.get('/leads', function(req, res, next) {
-  res.render('all', { title: 'DPLYR CRM' });
+router.get('/leads', async  function(req, res, next) {
+  var file = await fse.readJSONSync('/Users/ahmedmgh/WORK/DPLYR/Tech/utils/crm/data.json')
+
+  res.render('all', { title: 'DPLYR CRM', data: file });
 });
 router.post('/leads/add', LCtrl.addLead)
 module.exports = router;
