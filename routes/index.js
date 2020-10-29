08bee0e7b8568b6445e708783a670b5535a5281b
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-10-29 09:35:13 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var LCtrl = require('../controllers/leads')
 const fse = require('fs-extra');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'DPLYR CRM' });
 });
-router.get('/leads', async  function(req, res, next) {
+router.get('/leads', async function (req, res, next) {
   var file = await fse.readJSONSync('/Users/ahmedmgh/WORK/DPLYR/Tech/utils/crm/data.json')
-  file.forEach(function(v){ delete v.createdAtUS;  });
+  file.forEach(function (v) { delete v.createdAtUS; });
   res.render('all', { title: 'DPLYR CRM', data: file });
 });
 router.post('/leads/add', LCtrl.addLead)
