c72d7a4b62ec8c67d664b348f93a57fdc5938d2d
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-10-29 10:02:31 +0200

    fix issue in hello1 text

--- controllers/text.js
+++ controllers/text.js
@@ -3,7 +3,7 @@ var hello1 = function (lead){
   return `
   Hello ${lead.name}, 
   Hope you are staying safe, I was wondering what are you using in ${lead.company} as your cloud provider?
-  We have helped companies just like COMPANY reduce costs, be more flexible and iterate fast and I think we can help you too.
+  We have helped companies just like ${lead.company} reduce costs, be more flexible and iterate fast and I think we can help you too.
   
   
   `
